3af777feb2b637cc28c46f37fe578b84b5dc7dfa
thamisantos <tamimartins31113@gmail.com>
2022-11-17 22:52:29 -0300

    add estrutura api createuser

---
+++ src/API/api.js
@@ -0,0 +1,20 @@
+export const CreateUser = (name, email, password, role) => {
+    return fetch('lab-api-bq.onrender.com/users', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            password: password,
+            role: role,
+            restaurant: 'Burger Queen'
+
+        }
+            
+        )
+
+})
+
+}
